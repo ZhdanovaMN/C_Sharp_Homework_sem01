35b786e173c5cc634473744d58ef8ea62b7c2bbe
ZhdanovaMN <EyraM@yandex.ru>
2022-10-02 23:32:20 +0300

    Решеие задачи 2

---
+++ task02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userInput1);
+
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int b = int.Parse(userInput2);
+
+int max = a;
+
+if (a > b)
+{
+    Console.Write("a = " + a + ", b = " + b + " -> max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("a = " + a + ", b = " + b + " -> max = ");
+    Console.WriteLine(b);
+}
